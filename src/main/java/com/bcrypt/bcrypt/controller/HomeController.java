80376d8b91e91eeb9527b2ba09080dd6612eb147
raul <zarater.raul@gmail.com>
2022-06-05 16:37:22 -0700

    added some newer info

--- src/main/java/com/bcrypt/bcrypt/controller/HomeController.java
+++ src/main/java/com/bcrypt/bcrypt/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.bcrypt.bcrypt.controller;
 
+import com.bcrypt.bcrypt.model.Posts;
 import com.bcrypt.bcrypt.model.SiteUser;
 import com.bcrypt.bcrypt.repository.PostsRepository;
 import com.bcrypt.bcrypt.repository.SiteUserRepository;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController
@@ -27,5 +30,16 @@ public class HomeController
         return "home.html";
     }
 
+    @PostMapping("/add-post")
+    public RedirectView addPost(long siteUserId, String text)
+    {
+        SiteUser postsOfUser = siteUserRepository.getReferenceById(siteUserId);
+        Posts postsToAdd = new Posts(text);
+        postsToAdd.setPostsOfUser(postsOfUser);
+        postsRepository.save(postsToAdd);
+        String username = postsOfUser.getUsername();
+
+        return new RedirectView("/home/" + username);
+    }
 
 }
